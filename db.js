ab3f0609954c639ca27b0d429c164986e581a433
Andrew Riv <andrewriv@Mac.local>
2026-04-02 20:19:04 -0500

    Trim volume mount path whitespace

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Database = require('better-sqlite3');
 const path = require('path');
 const fs = require('fs');
 
-const dataDir = process.env.RAILWAY_VOLUME_MOUNT_PATH || path.join(__dirname, 'data');
+const dataDir = (process.env.RAILWAY_VOLUME_MOUNT_PATH || '').trim() || path.join(__dirname, 'data');
 const dbPath = path.join(dataDir, 'commissions.db');
 fs.mkdirSync(dataDir, { recursive: true });
 
